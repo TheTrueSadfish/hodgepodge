0d8f4a8ea50feec1061c241c847bb7c0a22a9cb6
Chairry <chairvaria@gmail.com>
2024-11-21 11:28:53 -0500

    Various Progress Checking Scripts implemented

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -673,7 +673,7 @@
 #define FLAG_RECEIVED_GOLD_TEETH  0x276 // Unused Flag
 #define FLAG_RECEIVED_TEA  0x277 // Unused Flag
 #define FLAG_HP_UNLOCKED_MUSEUM  0x278 // Unused Flag
-#define FLAG_UNUSED_0x279  0x279 // Unused Flag
+#define FLAG_HP_UNLOCKED_ITEM_MENU  0x279 // Unused Flag
 #define FLAG_UNUSED_0x27A  0x27A // Unused Flag
 #define FLAG_UNUSED_0x27B  0x27B // Unused Flag
 #define FLAG_UNUSED_0x27C  0x27C // Unused Flag
